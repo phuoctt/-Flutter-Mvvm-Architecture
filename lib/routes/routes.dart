02aca5eeb1b6049385b131aaada29f4285b1074f
Phuoc Tran <thephuoc2101@gmal.com>
2023-04-23 17:20:10 +0700

    add routes app

---
+++ lib/routes/routes.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mvvm/routes/routes_name.dart';
+import 'package:mvvm/view/home_screen.dart';
+import 'package:mvvm/view/login_screen.dart';
+
+class Routes {
+
+  static Route<dynamic>  generateRoute(RouteSettings settings){
+
+    switch(settings.name){
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (BuildContext context) => const HomeScreen());
+
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (BuildContext context) => const LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_){
+          return const Scaffold(
+            body: Center(
+              child: Text('No route'),
+            ),
+          );
+        });
+
+    }
+  }
+}
